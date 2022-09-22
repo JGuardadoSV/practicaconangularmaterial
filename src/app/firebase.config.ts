9f4acc2ef54d009c91bc4124e9a97c31c313f37a
Josue Guardado <jguardadosv@gmail.com>
2022-09-21 21:29:15 -0600

    autenticacion con google

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyA0dhRQyEqK2eClnOpSVBlFxuVVN6AtbAg",
+  authDomain: "proyectoclase04josue.firebaseapp.com",
+  projectId: "proyectoclase04josue",
+  storageBucket: "proyectoclase04josue.appspot.com",
+  messagingSenderId: "146732877662",
+  appId: "1:146732877662:web:af91732cbbd31a15ac71cf"
+};
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup } from "firebase/auth";
+import {Router} from "@angular/router";
+
+const provider = new GoogleAuthProvider();
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  loginGoogle() {
+  //***************************
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log("Datos del usuario "+JSON.stringify(user))
+
+        this.router.navigate(['listado'])
+        // ...
+      }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+
+
+    //****************************
+  }
 }
